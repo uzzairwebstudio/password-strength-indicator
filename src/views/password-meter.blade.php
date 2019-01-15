c7e62dc1d21d25de8d38a74b9a2bf30e95a5feb5
Uzzair Baharudin <uzzaircode@gmail.com>
2019-01-15 16:20:14 +0800

    update password-meter.blade.php

--- src/views/password-meter.blade.php
+++ src/views/password-meter.blade.php
@@ -1,3 +1,3 @@
 
-<meter max="4" id="password-strength-meter" class="col"></meter>
+<meter max="4" id="password-strength-meter" class="col" style="height:10px;padding-left:0px;padding-right:0px;"></meter>
 <p id="password-strength-text"></p>
